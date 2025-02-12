e4093bad4db525191b59169eaa30963ea5ed0d4b
aniket-kangane <aniketkangane@pepsales.co>
2025-02-12 15:41:29 +0530

    final commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET"],
+  })
+);
+
+// health route
+app.get("/health", (_, resp) => {
+  resp.status(200).json({
+    message: "Server is up and running",
+  });
+});
+
+// simple caching
+let users = [];
+
+app.get("/users", async (req, resp) => {
+  try {
+    const { name, sort } = req.query;
+
+    // handing  simple caching is not users then only fetch
+    if (!users.length) {
+      let response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      users = response.data;
+    }
+
+    if (name) {
+      const filteredUsersByName = users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase())
+      );
+      users = filteredUsersByName;
+    }
+
+    if (sort) {
+      if (sort == "asc") {
+        const sortedData = users.sort((a, b) => a.name.localeCompare(b.name));
+        users = sortedData;
+      } else if (sort == "desc") {
+        const sortedData = users.sort((a, b) => b.name.localeCompare(a.name));
+        users = sortedData;
+      } else {
+        console.log("sort is not provided");
+      }
+    }
+
+    resp.status(200).json({
+      users,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server is runing on port ${PORT}`);
+});
